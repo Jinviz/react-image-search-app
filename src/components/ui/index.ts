48e057c3f3b4cc3f9a860107a713b26dce50df56
Ray <rlawlsdn9583@gmail.com>
2024-11-15 11:21:50 +0900

    chore: 프로젝트 구조 및 세팅

---
+++ src/components/ui/index.ts
@@ -0,0 +1,29 @@
+// UI 모듈
+export { Separator } from './separator'
+export { Skeleton } from './skeleton'
+export { Button, buttonVariants } from './button'
+export { Input } from './input'
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from './pagination'
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from './dialog'
+
+export { Avatar, AvatarImage, AvatarFallback } from './avatar'
---
+++ src/views/Bookmark.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookMarkPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BookMarkPage
